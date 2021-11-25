44fe3366563fe9f80f569ce26556b4ad42e65981
radwanromy <radwanromy@gmail.com>
2021-11-25 17:17:51 +0600

    Ball AI 037

--- ball.lua
+++ ball.lua
@@ -22,12 +22,32 @@ function Ball:collide()
     local collisionPosition = middleBall - middlePlayer
     self.yVel = collisionPosition * 5
   end
+  if checkCollision(self, AI) then
+    self.xVel = -self.speed
+    local middleBall = self.y + self.height / 2
+    local middleAI = AI.y + AI.height / 2
+    local collisionPosition = middleBall - middleAI
+    self.yVel = collisionPosition * 5
+  end
   if self.y< 0 then
     self.y = 0
     self.yVel = -self.yVel
   elseif self.y + self.height> love.graphics.getHeight() then
     self.y = love.graphics.getHeight() - self.height
     self.yVel = -self.yVel
+  end
+
+  if self.x< 0 then
+    self.x = love.graphics.getWidth()/2 - self.width / 2
+    self.y = love.graphics.getHeight()/2 - self.height / 2
+    self.yVel= 0
+    self.xVel= self.speed
+  end
+  if self.x + self.width > love.graphics.getWidth() then
+    self.x = love.graphics.getWidth()/2 - self.width / 2
+    self.y = love.graphics.getHeight()/2 - self.height / 2
+    self.yVel= 0
+    self.xVel= self.speed
   end
 
 end
