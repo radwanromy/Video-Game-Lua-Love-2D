5a0f357e686bd3cf5832a4627fd39be4285d63d9
radwanromy <radwanromy@gmail.com>
2021-11-25 12:38:11 +0600

    021

--- main.lua
+++ main.lua
@@ -2,12 +2,15 @@ require("player")
 
 function love.load()
   Player:load()
+  Ball:load()
 end
 
 function love.update(dt)
   Player:update(dt)
+  Ball:update(dt)
 end
 
 function love.draw()
   Player:draw()
+  Ball:draw()
 end
