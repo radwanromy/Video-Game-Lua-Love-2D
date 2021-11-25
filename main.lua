5515c113b69e2e391cecff2d51142a8effffe249
radwanromy <radwanromy@gmail.com>
2021-11-25 15:48:37 +0600

    Main 031

--- main.lua
+++ main.lua
@@ -4,16 +4,19 @@ require("ball")
 function love.load()
   Player:load()
   Ball:load()
+  AI:load()
 end
 
 function love.update(dt)
   Player:update(dt)
   Ball:update(dt)
+  AI:update(dt)
 end
 
 function love.draw()
   Player:draw()
   Ball:draw()
+  AI:draw()
 end
 function checkCollision(a,b)
   if a.x + a.width > b.x and a.x < b.x + b.width and a.y +a.height> b.y and a.y < b.y + b.height then
