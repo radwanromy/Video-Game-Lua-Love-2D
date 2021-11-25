d5fa86c373df360f813142686413a35a8f50f109
radwanromy <radwanromy@gmail.com>
2021-11-25 17:06:59 +0600

    AI 035

--- ai.lua
+++ ai.lua
@@ -9,10 +9,21 @@ function AI:load()
   self.speed = 500
 end
 function AI:update(dt)
-
+  self:move(dt)
 end
 
-
+function AI:move(dt)
+  self.y = self.y + self.yVel *dt
+end
+function AI:acquireTarget()
+  if Ball.y + Ball.height< self.y then
+    self.yVel = -self.speed
+  elseif Ball.y > self.y + self.height then
+    self.yVel = self.speed
+  else
+    self.yVel = 0
+  end
+end
 
 function AI:draw()
   love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
