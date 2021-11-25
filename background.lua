b65192c2ace48631f6d8f0ceeab6f2ecbd442cbc
radwanromy <radwanromy@gmail.com>
2021-11-25 18:17:07 +0600

    Background Lua 045

--- background.lua
+++ background.lua
@@ -3,10 +3,17 @@
 Background = {}
 
 function Background:load()
+   self.universe = love.graphics.newImage("assets/universe.png")
+    self.planets = love.graphics.newImage("assets/universe.png")
+    self.width = self.planets:getWidth()
+    self.height= self.planets:getHeight()
+    self.rotation = 0
 
 end
 function Background:update(dt)
-
+  self.rotation= self.rotation + 0.05 * dt
 end
 function Background:draw()
+  love.graphics.draw(self.universe,0,0)
+  love.graphics.draw(self.planets,self.width/2,self.width/2,self.rotation,1,1,self.width/2,self.height/2)
 end
