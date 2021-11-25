3886ed94cc580c6f5796ac8d9d8d773878e91c99
radwanromy <radwanromy@gmail.com>
2021-11-25 10:34:27 +0600

    Lua Love 2d 012

--- player.lua
+++ player.lua
@@ -1,12 +1,17 @@
 Player = {}
 
 function Player:load()
-
+  self.x=50
+  self.y = love.graphics.getHeight()/2
+  self.width = 20
+  self.height= 100
+  self.speed = 500
 end
 function Player:update(dt)
 
 end
 
 function Player:draw()
+  love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
 
 end
