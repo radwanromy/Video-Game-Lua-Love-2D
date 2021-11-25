5a8df281f3247dd763bff3904979f040db73efb9
radwanromy <radwanromy@gmail.com>
2021-11-25 17:49:58 +0600

    Ball Assets Use 041

--- ball.lua
+++ ball.lua
@@ -3,8 +3,9 @@ Ball = {}
 function Ball:load()
   self.x = love.graphics.getWidth()/2
   self.y = love.graphics.getHeight()/2
-  self.width = 20
-  self.height = 20
+  self.img = love.graphics.newImage("assets/ball.png")
+  self.width = self.img:getWidth()
+  self.height= self.img:getHeight()
   self.speed = 200
   self.xVel = -self.speed
   self.yVel = 0
@@ -58,5 +59,5 @@ self.y = self.y + self.yVel *dt
 end
 
 function Ball:draw()
-  love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
+  love.graphics.draw(self.img,self.x,self.y)
 end
