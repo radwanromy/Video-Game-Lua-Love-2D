c977b082e0113b863b809684ca48d73fa333a30b
radwanromy <radwanromy@gmail.com>
2021-11-25 13:02:19 +0600

    023

--- main.lua
+++ main.lua
@@ -15,3 +15,10 @@ function love.draw()
   Player:draw()
   Ball:draw()
 end
+function checkCollision(a,b)
+  if a.x + a.width > b.x and a.x < b.x + b.width and a.y +a.height> b.y and a.y < b.y + b.height then
+    return true
+  else
+    return false
+    
+end
