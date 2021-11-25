3985a1667b9eba4549dec807595007e57438bd41
radwanromy <radwanromy@gmail.com>
2021-11-25 10:50:23 +0600

    017

--- player.lua
+++ player.lua
@@ -8,19 +8,29 @@ function Player:load()
   self.speed = 500
 end
 function Player:update(dt)
-   if love.keyboard.isDown("w") then
-     self.y = self.y - self.speed * dt
-  elseif love.keyboard.isDown("s") then
-    self.y = self.y + self.speed * dt
-  end
 
+  self:move(dt)
+  self:checkBoundaries()
+
+
+end
+
+function Player:move(dt)
+  if love.keyboard.isDown("w") then
+    self.y = self.y - self.speed * dt
+ elseif love.keyboard.isDown("s") then
+   self.y = self.y + self.speed * dt
+ end
+end
+
+function Player:checkBoundaries()
   if self.y <0 then
     self.y = 0
   elseif self.y + self.height> love.graphics.getHeight() then
     self.y = love.graphics.getHeight() - self.height
   end
-
 end
+
 
 function Player:draw()
   love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
