446409ee334c3a74b7fdc1f1948714b05cb0f4ff
radwanromy <radwanromy@gmail.com>
2021-11-25 18:03:30 +0600

    Collide Ball 043

--- ball.lua
+++ ball.lua
@@ -16,6 +16,25 @@ function Ball:update(dt)
   self:collide()
 end
 function Ball:collide()
+  self:collideWall()
+  self:collidePlayer()
+  self:collideAI()
+  self:score()
+end
+
+function Ball:collideWall()
+
+    if self.y< 0 then
+      self.y = 0
+      self.yVel = -self.yVel
+    elseif self.y + self.height> love.graphics.getHeight() then
+      self.y = love.graphics.getHeight() - self.height
+      self.yVel = -self.yVel
+    end
+
+end
+
+function Ball:collidePlayer()
   if checkCollision(self, Player) then
     self.xVel = self.speed
     local middleBall = self.y + self.height / 2
@@ -23,6 +42,9 @@ function Ball:collide()
     local collisionPosition = middleBall - middlePlayer
     self.yVel = collisionPosition * 5
   end
+end
+
+function Ball:collideAI()
   if checkCollision(self, AI) then
     self.xVel = -self.speed
     local middleBall = self.y + self.height / 2
@@ -30,27 +52,22 @@ function Ball:collide()
     local collisionPosition = middleBall - middleAI
     self.yVel = collisionPosition * 5
   end
-  if self.y< 0 then
-    self.y = 0
-    self.yVel = -self.yVel
-  elseif self.y + self.height> love.graphics.getHeight() then
-    self.y = love.graphics.getHeight() - self.height
-    self.yVel = -self.yVel
-  end
+end
 
+function Ball:score()
   if self.x< 0 then
-    self.x = love.graphics.getWidth()/2 - self.width / 2
-    self.y = love.graphics.getHeight()/2 - self.height / 2
-    self.yVel= 0
-    self.xVel= self.speed
+    self:resetPosition(1)
   end
   if self.x + self.width > love.graphics.getWidth() then
-    self.x = love.graphics.getWidth()/2 - self.width / 2
-    self.y = love.graphics.getHeight()/2 - self.height / 2
-    self.yVel= 0
-    self.xVel= self.speed
+    self:resetPosition(-1)
   end
+end
 
+function Ball:resetPosition(modifier)
+  self.x = love.graphics.getWidth()/2 - self.width / 2
+  self.y = love.graphics.getHeight()/2 - self.height / 2
+  self.yVel= 0
+  self.xVel= self.speed * modifier
 end
 
 function Ball:move(dt)
