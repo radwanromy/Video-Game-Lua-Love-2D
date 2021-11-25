98cd62b4be20c82458dba7f51c9395fbdde70178
radwanromy <radwanromy@gmail.com>
2021-11-25 15:55:05 +0600

    AI 032

--- ai.lua
+++ ai.lua
@@ -1,11 +1,16 @@
 AI= {}
 
 function AI:load()
-
+  self.width = 20
+  self.height = 100
+  self.x = lover.graphics.getWidth() - self.width - 50
+  self.y = love.graphics.getHeight()/2
+  self.yVel = 0
+  self.speed = 500
 end
 function AI:update(dt)
 
 end
 function AI:draw()
-
+  love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
 end
