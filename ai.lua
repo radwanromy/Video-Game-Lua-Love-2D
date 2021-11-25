be5fa51c7f285c7bfbcd4abb3487fc44a7e4bc2c
radwanromy <radwanromy@gmail.com>
2021-11-25 17:11:04 +0600

    AI 036

--- ai.lua
+++ ai.lua
@@ -7,9 +7,17 @@ function AI:load()
   self.y = love.graphics.getHeight()/2
   self.yVel = 0
   self.speed = 500
+  self.timer = 0
+  self.rate = 0.5
+
 end
 function AI:update(dt)
   self:move(dt)
+  self.timer = self.timer + dt
+  if self.timer > self.rate then
+    self.timer = 0
+    self:acquireTarget()
+  end
 end
 
 function AI:move(dt)
