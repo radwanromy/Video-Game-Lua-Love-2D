1cad8566a80d89b682266d3d9ba1a4f0d6d73797
radwanromy <radwanromy@gmail.com>
2021-11-25 10:45:31 +0600

    Lua Love 2D 016

--- player.lua
+++ player.lua
@@ -14,6 +14,12 @@ function Player:update(dt)
     self.y = self.y + self.speed * dt
   end
 
+  if self.y <0 then
+    self.y = 0
+  elseif self.y + self.height> love.graphics.getHeight() then
+    self.y = love.graphics.getHeight() - self.height
+  end
+
 end
 
 function Player:draw()
