0337265e49cf22035c206ca8633a765124a32c1b
radwanromy <radwanromy@gmail.com>
2021-11-25 18:25:24 +0600

    main Lua final commit

--- main.lua
+++ main.lua
@@ -1,23 +1,28 @@
 require("player")
 require("ball")
 require("ai")
+require("background")
 
 function love.load()
   Player:load()
   Ball:load()
   AI:load()
+  Background:load()
 end
 
 function love.update(dt)
   Player:update(dt)
   Ball:update(dt)
   AI:update(dt)
+  Background:update(dt)
 end
 
 function love.draw()
+  Background:draw()
   Player:draw()
   Ball:draw()
   AI:draw()
+
 end
 function checkCollision(a,b)
   if a.x + a.width > b.x and a.x < b.x + b.width and a.y +a.height> b.y and a.y < b.y + b.height then
